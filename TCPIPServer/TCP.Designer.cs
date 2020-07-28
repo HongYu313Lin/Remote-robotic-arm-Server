bb033d36e7d633c736cb1840b8e8c2d3f91ff049
Hong-Yu,Lin <4a12c062@stust.edu.tw>
2020-07-28 22:59:30 +0800

    加入專案檔案。

---
+++ TCPIPServer/TCP.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace TCPIPServer
+{
+    partial class TCP
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textIPnum = new System.Windows.Forms.TextBox();
+            this.textportnum = new System.Windows.Forms.TextBox();
+            this.textmessage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonenter = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // textIPnum
+            // 
+            this.textIPnum.Location = new System.Drawing.Point(49, 11);
+            this.textIPnum.Margin = new System.Windows.Forms.Padding(4);
+            this.textIPnum.Name = "textIPnum";
+            this.textIPnum.Size = new System.Drawing.Size(108, 25);
+            this.textIPnum.TabIndex = 0;
+            // 
+            // textportnum
+            // 
+            this.textportnum.Location = new System.Drawing.Point(226, 11);
+            this.textportnum.Margin = new System.Windows.Forms.Padding(4);
+            this.textportnum.Name = "textportnum";
+            this.textportnum.Size = new System.Drawing.Size(108, 25);
+            this.textportnum.TabIndex = 0;
+            // 
+            // textmessage
+            // 
+            this.textmessage.Location = new System.Drawing.Point(13, 44);
+            this.textmessage.Margin = new System.Windows.Forms.Padding(4);
+            this.textmessage.Multiline = true;
+            this.textmessage.Name = "textmessage";
+            this.textmessage.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textmessage.Size = new System.Drawing.Size(428, 165);
+            this.textmessage.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(9, 13);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(172, 13);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Port";
+            // 
+            // buttonenter
+            // 
+            this.buttonenter.Location = new System.Drawing.Point(349, 11);
+            this.buttonenter.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonenter.Name = "buttonenter";
+            this.buttonenter.Size = new System.Drawing.Size(92, 25);
+            this.buttonenter.TabIndex = 2;
+            this.buttonenter.Text = "Set";
+            this.buttonenter.UseVisualStyleBackColor = true;
+            this.buttonenter.Click += new System.EventHandler(this.buttonenter_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // TCP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(653, 328);
+            this.Controls.Add(this.buttonenter);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textmessage);
+            this.Controls.Add(this.textportnum);
+            this.Controls.Add(this.textIPnum);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "TCP";
+            this.Text = "TCP";
+            this.Load += new System.EventHandler(this.TCP_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textIPnum;
+        private System.Windows.Forms.TextBox textportnum;
+        private System.Windows.Forms.TextBox textmessage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonenter;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ TCPIPServer/TCP.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using XYZrobot;
+using System.Threading;
+using System.Diagnostics;
+namespace TCPIPServer
+{
+    public partial class TCP : Form
+    {
+        XYZrobot.XYZrobot robotarm;
+        public TCP()
+        {
+            InitializeComponent();
+            robotarm = new XYZrobot.XYZrobot();
+            robotarm.Show();
+
+            textIPnum.Text = "127.0.0.1";
+
+            textportnum.Text = "13000";
+        }
+
+        int port;//port型態
+
+        String IP ;//IP型態
+
+        TcpListener myTcpListener;
+
+        Socket mySocket;
+
+        private void buttonenter_Click(object sender, EventArgs e)
+        {
+            if (!backgroundWorker1.IsBusy)
+            {
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void TCP_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            IP = textIPnum.Text;
+            port = Convert.ToInt16(textportnum.Text);
+            IPAddress theIPAddress = IPAddress.Parse(IP);
+            myTcpListener = new TcpListener(theIPAddress, port);
+            this.Invoke(new EventHandler(delegate
+            {
+                textmessage.Text += System.DateTime.Now.ToString() + Environment.NewLine + "IP:" + IP + "通訊埠:" + port + Environment.NewLine + "等待用戶端連線.. ";
+                textmessage.AppendText("..." + "\r\n");
+            }));
+            myTcpListener.Start();
+
+            mySocket = myTcpListener.AcceptSocket();
+
+            try
+            {
+                if (mySocket.Connected)
+                {
+                    IPEndPoint remoteIpEndPoint = mySocket.RemoteEndPoint as IPEndPoint;
+                    IPEndPoint localIpEndPoint = mySocket.LocalEndPoint as IPEndPoint;
+                    this.Invoke(new EventHandler(delegate
+                    {
+                        textmessage.Text += Environment.NewLine + System.DateTime.Now.ToString() + Environment.NewLine + remoteIpEndPoint.Address.ToString() + "  連線成功!! ";//用戶端請求連線成功，就會秀出訊息。
+
+                        textmessage.AppendText("..." + "\r\n");
+                    }));
+
+                }
+
+
+            }
+            catch (Exception Q)
+            {
+                this.Invoke(new EventHandler(delegate
+                {
+                    textmessage.Text = Q.ToString();
+                }));
+                //throw;
+            }
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            while (true)
+            {
+                try
+                {
+
+                    if (mySocket.Connected == false || sw.Elapsed.TotalSeconds>5)
+                    {
+                        this.Invoke(new EventHandler(delegate { 
+                            textmessage.Text += Environment.NewLine + "連線終止";
+                            textmessage.AppendText("..." + "\r\n");
+                            myTcpListener.Stop();
+                         }));
+                         break;
+                    }
+                    
+                    if (mySocket.Available >= 1)
+                    {
+                        sw.Restart();
+                        byte[] myBufferBytes = new byte[mySocket.Available];
+
+                        int dataLength = mySocket.Receive(myBufferBytes); //取得用戶端寫入的資料
+                        
+                        XYZrobot.XYZrobot.Button temp = XYZrobot.XYZrobot.Button.Ping;
+                        for (int i = dataLength-1; i >= 0; i--)
+                        {
+                            if ( (XYZrobot.XYZrobot.Button)myBufferBytes[i]!=XYZrobot.XYZrobot.Button.Ping)
+                            {
+                                temp = (XYZrobot.XYZrobot.Button)myBufferBytes[dataLength - 1];
+                                break;
+                            }
+                        }
+                        robotarm.btn = temp;
+                        
+                       
+                        this.Invoke(new EventHandler(delegate { 
+
+                        textmessage.Text += Environment.NewLine + System.DateTime.Now.ToString() + Environment.NewLine + "接收到的資料長度 = " + dataLength.ToString();
+
+                        textmessage.Text += Environment.NewLine + "取出用戶端寫入網路資料流的資料內容" + Environment.NewLine + Encoding.ASCII.GetString(myBufferBytes, 0, dataLength);
+
+                        textmessage.AppendText("..." + "\r\n");
+
+                        
+                        }));
+                        mySocket.Send(myBufferBytes, myBufferBytes.Length, 0);//將接收到的資料回傳給用戶端(Clinet)
+                    }
+                }
+                catch (System.Net.Sockets.SocketException eh)
+                {
+                     this.Invoke(new EventHandler(delegate { 
+                        textmessage.Text += Environment.NewLine + "連線終止";
+                        textmessage.AppendText("..." + "\r\n");
+                        myTcpListener.Stop();
+                     }));
+                     break;
+                }
+                Thread.Sleep(10);
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+
+    }
+}
